d9fcb6992cd7c92f3be29f99d704788e6a2ecc73
duycan17 <duydk.22git@vku.udn.vn>
2023-12-04 12:42:52 +0700

    merge

--- src/main/java/net/codejava/controller/UserController.java
+++ src/main/java/net/codejava/controller/UserController.java
@@ -2,7 +2,6 @@ package net.codejava.controller;
 
 import net.codejava.dto.modifyUser.ChangeAvatarDTO;
 import net.codejava.dto.modifyUser.ChangePasswordDTO;
-import net.codejava.dto.modifyUser.ChangeProDTO;
 import net.codejava.entity.User;
 import net.codejava.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,28 +10,28 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-
 @RolesAllowed("USER")
 @CrossOrigin
-
 public class UserController {
     @Autowired
     private UserService userService;
+
     @RolesAllowed("USER")
     @PatchMapping("/password")
     public ResponseEntity<User> changePassword(@RequestBody ChangePasswordDTO changePasswordDTO, Principal principal) {
         User user = userService.changePassword(changePasswordDTO.getOldPassword(), changePasswordDTO.getPassword(), principal);
         return ResponseEntity.ok(user);
     }
+
     @RolesAllowed("USER")
     @PatchMapping("/avatar")
     public ResponseEntity<User> changeAvatar(@RequestBody ChangeAvatarDTO changeAvatarDTO, Principal principal) {
         return ResponseEntity.ok(userService.changeAvatar(changeAvatarDTO.getUrl(), principal));
     }
+
     @RolesAllowed("ADMIN")
     @PatchMapping("/pro")
     public ResponseEntity<User> changePro(@RequestParam("id") Long userId) {
